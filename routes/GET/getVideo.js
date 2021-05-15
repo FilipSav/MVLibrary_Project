c408dac93e92df89f4c6c3f65bc51606e84d5c68
Filip Savčenko <99filipsav@seznam.cz>
2021-05-15 15:53:50 +0200

    updateing getVideo

--- routes/GET/getVideo.js
+++ routes/GET/getVideo.js
@@ -1,15 +1,19 @@
-const { get } = require("mongoose");
-
 const getVideo = require("express").Router();
+const videos = require("../../models/video")
 getVideo.get("/get-video",(req,res) => {
-    res.json({
-        msg:"List of videos",
-        listOfVideos:[
-            {video:"first"},
-            {video:"second"},
-            {video:"third"}
-        ]
-    })
+   videos.find({},(err,docs) => {
+       if(err){
+           return res.json({
+               msg:"Wasnt able to get video",
+               documents:[]
+           })
+       }else{
+           return res.json({
+               msg:"Was succesfuly able to get video",
+               documents:docs
+           })
+       }
+   })
 })
 
 module.exports = getVideo;
