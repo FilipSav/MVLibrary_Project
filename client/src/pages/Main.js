65646ad6e28eee7fe331d4ed4f6fabad7f5545d5
Filip Savčenko <99filipsav@seznam.cz>
2021-05-16 07:56:10 +0200

    react add video

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 const Main = () => {
+  const [video, setVideo] = useState([
+    {title:"muzika"},
+    {artist:"John"},
+    {album:"you need to save"},
+    {year:1980},
+    {description:"je to hudba"},
+    {link:"www.seznam.cz"}
+  ]);
+  const addingVideo = () => {
+    const array = [...video];
+    array.push({title:"muzika"});
+    array.push({artist:"Adam"});
+    array.push({album:"Helo helo"});
+    array.push({year:1980});
+    array.push({description:"je to písnička"});
+    array.push({link:"www.google.com"});
+    setVideo(array);
+  }
   return (
     <div>
-    <p>Main page</p>
+      {
+        video.map((video,index) => {
+          return (
+            <div key={index}>{video.title}{video.artist}
+            {video.album}{video.year}{video.description}{video.link}</div>
+            
+          )
+          
+        })
+      }
+      <div className="btn" onClick={addingVideo}>Add video</div>
     </div>
   );
 }
